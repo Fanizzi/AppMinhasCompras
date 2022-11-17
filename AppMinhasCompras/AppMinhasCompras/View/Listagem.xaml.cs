b94d30f0669d11a58fe5d386e6cb896882e01321
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2022-11-16 23:55:10 -0300

    add Listagem de produtos

--- AppMinhasCompras/AppMinhasCompras/View/Listagem.xaml.cs
+++ AppMinhasCompras/AppMinhasCompras/View/Listagem.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AppMinhasCompras.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +14,15 @@ namespace AppMinhasCompras.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Listagem : ContentPage
     {
+        ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
         public Listagem()
         {
             InitializeComponent();
+
+            lst_produtos.ItemsSource = lista_produtos;
         }
 
-        private void ToolbarItem_ClickedNovo (object sender, EventArgs e)
+        private void ToolbarItem_Clicked_Novo (object sender, EventArgs e)
         {
             try
             {
@@ -29,9 +34,74 @@ namespace AppMinhasCompras.View
             }
         }
 
-        private void ToolbarItem_ClickedSomar(object sender, EventArgs e)
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
         {
+            double soma = lista_produtos.Sum(i => i.Preco * i.Quantidade);
 
+            string msg = "O total da compra é: " + soma;
+
+            DisplayAlert("Atenção!", msg, "OK");
+        }
+
+        protected override void OnAppearing()
+        {
+            if(lista_produtos.Count == 0)
+            {
+                System.Threading.Tasks.Task.Run(async () =>
+                {
+                    List<Produto> temp = await App.Database.GetAll();
+
+                    foreach (Produto item in temp)
+                    {
+                        lista_produtos.Add(item);
+                    }
+
+                    ref_carregando.IsRefreshing = false;
+                });
+            }
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem disparador = (MenuItem)sender;
+
+            Produto produto_selecionado = (Produto)disparador.BindingContext;
+
+            bool confirmacao = await DisplayAlert("Tem Certeza?", "Remover Item?", "Sim", "Não");
+
+            if (confirmacao)
+            {
+                await App.Database.Delete(produto_selecionado.Id);
+
+                lista_produtos.Remove(produto_selecionado);
+            }
+        }
+
+        private void txt_busca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string busca = e.NewTextValue;
+
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.Search(busca);
+
+                lista_produtos.Clear();
+
+                foreach (Produto item in temp)
+                {
+                    lista_produtos.Add(item);
+                }
+
+                ref_carregando.IsRefreshing = false;
+            });
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Navigation.PushAsync(new EditarProduto
+            {
+                BindingContext = (Produto)e.SelectedItem
+            });
         }
     }
 }
